75c443d2c9f28250e40ef05af145dc042962c55c
Margarita <34695872+MargaritaRay@users.noreply.github.com>
2018-03-19 13:39:38 +0300

    Update index.php

--- 1,2/index.php
+++ 1,2/index.php
@@ -1,107 +1,40 @@
 <?php
-
-
-/*class GuestBook{
-
-    protected $way;
-    public $text;
+class GuestBook
+{
+    protected $data;
+    public $way;
 
     function __construct($way)
     {
-    $this->way = $way;
-    return readfile($this->way);
-    }
-
-    public function getData(){
-        return file($this->way);
-    }
-
-    public function append($text){
-        return array_push($this->getData(),  $text);
-    }
-
-    public function save(){
-        return file_put_contents($this->way, $this->text, FILE_APPEND);
-    }
-
-}
-
-$book = new GuestBook(__DIR__ . '/db.txt');
-$book->text = 'Новый текст'."\n";
-$book->getData();
-$book->save();*/
-
-//==================2=======================
-
-class TextFile{
-    protected $way;
-
-
-    function __construct($way)
-    {
+        //путь собираетя из обьекта
         $this->way = $way;
-        return readfile($this->way);
+        //читает содержимое файла, помещает в массиа
+        $this->data = file($way);
     }
-}
 
-
-
-class GuestBook extends TextFile {
-
-    public $text;
-
-    function __construct($way)
+    public function getData()
     {
-        parent::__construct($way);
+        //возвращает массив записей (содержимое файла)
+       return  $this->data;
     }
 
-    public function getData(){
-        return file($this->way);
+    public function append($text)
+    {
+        //добавление элимента в конец массива (содержимое файла, элемент)
+         array_push($this->data,  $text);
     }
 
-    public function append($text){
-        return array_push($this->getData(),  $text);
-    }
+    public function  save()
+    {
+        //запись в файл
+        file_put_contents($this->way, $this->data);
 
-    public function save(){
-         file_put_contents($this->way, $this->text, FILE_APPEND);
     }
 
 }
 
 $book = new GuestBook(__DIR__ . '/db.txt');
-$book->text = "\n". 'Новый текст ';
-$book->getData();
+$book->append( "\n".'Новый текст ');
 $book->save();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo implode("\n",$book->getData())."<br>";
